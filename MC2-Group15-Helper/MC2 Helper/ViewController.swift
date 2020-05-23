d1d4b1b1b81902e7ab82577092e397dbbc1a3ac8
griff88 <griffpoldi@gmail.com>
2020-05-23 20:01:32 +0700

    storyboard helper
    Helper terima pesanan

---
+++ MC2-Group15-Helper/MC2 Helper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC2 Elder
+//
+//  Created by Griffin on 22/05/20.
+//  Copyright © 2020 Griffin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
