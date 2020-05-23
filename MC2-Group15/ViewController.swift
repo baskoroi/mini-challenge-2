9f2c36e55d0ea9c63ba8db8b066511a3e9c2646b
Angel <angela.fanuela@gmail.com>
2020-05-23 15:14:08 +0700

    Storyboard utk elder halaman 1-6

---
+++ MC2-Group15/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MC2-Elder
+//
+//  Created by Angela Fanuela on 23/05/20.
+//  Copyright © 2020 Angela Fanuela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
