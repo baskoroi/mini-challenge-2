0440fd4df0143d2fc1d520e7f1ee4c07ccfa62bc
Faris Ali Yafie <farisaliyafie@gmail.com>
2020-05-28 20:34:15 +0700

    sudah jalan, add controller posisi and lokasi anda. Blm masuk code mapnya

---
+++ MC2-Group15/Controllers/LokasiAndaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LokasiAndaViewController.swift
+//  MC2-Group15
+//
+//  Created by Faris Ali Yafie on 28/05/20.
+//  Copyright © 2020 Angela Fanuela. All rights reserved.
+//
+
+import UIKit
+
+class LokasiAndaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
