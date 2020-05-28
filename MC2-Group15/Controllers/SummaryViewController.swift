43e3c2ef4e14c3b7caaf2519953529bab8a2b423
Baskoro Indrayana <baskoroindrayana@gmail.com>
2020-05-28 10:30:07 +0700

    Added missing view controllers: shopping lists + summary

---
+++ MC2-Group15/Controllers/SummaryViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SummaryViewController.swift
+//  MC2Prototype-ShoppingList
+//
+//  Created by Baskoro Indrayana on 05/27/20.
+//  Copyright © 2020 Baskoro Indrayana. All rights reserved.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+
+    var shoppingList: CuratedShoppingList?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let list = shoppingList {
+            print(list)
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
